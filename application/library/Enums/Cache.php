be0d19edb71d39bb3edacfcbe23e6842634e9cc7
王旋 <bigmore@qq.com>
2018-11-12 11:48:57 +0800

    modify cache time

--- application/library/Enums/Cache.php
+++ application/library/Enums/Cache.php
@@ -5,7 +5,7 @@ namespace Enums;
 class Cache
 {
     const RSS_KEY_TEMPLATE = 'RSS:%s_%s_%s_%s';
-    const RSS_EXPIRE_TIME  = 10 * 60;
+    const RSS_EXPIRE_TIME  = 10 * 1;
 
     const RSS_DETAIL_KEY_TEMPLATE = 'RSS_DETAIL:%s_%s_%s';
     const RSS_DETAIL_EXPIRE_TIME  = 10 * 60;
